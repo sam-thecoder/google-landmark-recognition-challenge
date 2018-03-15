efe929768d0ffe2e5809e5aac7f07da328ba2521
Samuel M <muiruri.samuel@gmail.com>
2018-03-15 14:22:45 +0300

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
 
 
 # dimensions of our images.
