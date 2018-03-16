3e6cf287edeef4866c481422dc4b1bf13a06b1bb
Samuel M <muiruri.samuel@gmail.com>
2018-03-16 09:44:14 +0300

    Create train.py

---
+++ train.py
@@ -0,0 +1,87 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense, Input, Model
+from keras import backend as K
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+
+import numpy as np
+import os
+import time
+from resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.utils import np_utils
+
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = 'training_images'
+validation_data_dir = 'validation_images'
+nb_train_samples = 2000
+nb_validation_samples = 800
+epochs = 50
+batch_size = 16
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+    
+num_classes = 14951
+
+model = ResNet50(input_tensor=image_input, include_top=True,weights='imagenet')
+model.summary()
+last_layer = model.get_layer('avg_pool').output
+x= Flatten(name='flatten')(last_layer)
+out = Dense(num_classes, activation='softmax', name='output_layer')(x)
+custom_resnet_model = Model(inputs=image_input,outputs= out)
+custom_resnet_model.summary()
+
+for layer in custom_resnet_model.layers[:-1]:
+	layer.trainable = False
+
+custom_resnet_model.layers[-1].trainable
+
+custom_resnet_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=5, verbose=0, mode='auto')
+checkpointer = ModelCheckpoint(filepath="best_weights.hdf5", verbose=0, save_best_only=True) # save best model
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+custom_resnet_model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_steps=nb_validation_samples // batch_size,
+    callbacks=[monitor,checkpointer], 
+    validation_data=validation_generator)
+
+custom_resnet_model.load_weights('best_weights2.hdf5') # load weights from best model
+custom_resnet_model.save('save_model.h5')
+   
