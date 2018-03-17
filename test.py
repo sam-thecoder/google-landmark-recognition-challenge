774230d8813826d3888aa7cfcaf13f14488d3ee1
Samuel M <muiruri.samuel@gmail.com>
2018-03-17 13:30:12 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+from keras.models import load_model
+import numpy
+
+model = load_model('last_model.h5')
+
+# dimensions of our images.
+img_width, img_height = 250, 250
+
+test_data_dir = 'test_images'
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(img_width, img_height))
+    
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+predict = model.predict_generator(test_generator)
+
+np.save('prediction.npy', predict)
