af213908dbce1f2a8de6cb42d413824cf3cd79fb
Samuel M <muiruri.samuel@gmail.com>
2018-03-19 09:14:00 +0300

    Update train.py

--- train.py
+++ train.py
@@ -51,12 +51,11 @@ train_datagen = ImageDataGenerator(
     rescale=1. / 255,
     shear_range=0.2,
     zoom_range=0.2,
-    batch_size=29902,
     horizontal_flip=True)
 
 # this is the augmentation configuration we will use for testing:
 # only rescaling
-test_datagen = ImageDataGenerator(rescale=1. / 255, batch_size=29902)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
